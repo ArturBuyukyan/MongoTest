031181e732b9a8281f6dd09dec4307a7d21f2e8c
Valerij Sidorenko <rezonans65@gmail.com>
2017-09-25 10:53:14 +0300

    added json stringify

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from "meteor/meteor";
-
+var ObjectID = require("mongodb").ObjectID;
 Meteor.startup(() => {
   payCollection = new Mongo.Collection("payCollection");
   var match = payCollection.aggregate(
@@ -40,5 +40,39 @@ Meteor.startup(() => {
     ],
     { allowDiskUse: true }
   );
-  console.log(match);
+  console.log("Explain Report:", JSON.stringify(match));
+  console.log("Group Count Result:", Object.keys(match).length);
+  console.log("Collection Row Count:", payCollection.find().count());
+  // function randomString(length, chars) {
+  //   var result = "";
+  //   for (var i = length; i > 0; --i)
+  //     result += chars[Math.floor(Math.random() * chars.length)];
+  //   return result;
+  // }
+  // function randomInteger(min, max) {
+  //   var rand = min - 0.5 + Math.random() * (max - min + 1);
+  //   rand = Math.round(rand);
+  //   return rand;
+  // }
+  // var mongoObjectId = function() {
+  //   var timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
+  //   return (
+  //     timestamp +
+  //     "xxxxxxxxxxxxxxxx"
+  //       .replace(/[x]/g, function() {
+  //         return ((Math.random() * 16) | 0).toString(16);
+  //       })
+  //       .toLowerCase()
+  //   );
+  // };
+  // for (i = 0; i < 200000; i++) {
+  //   payCollection.insert({
+  //     id: i,
+  //     userId: randomInteger(10000, 199999),
+  //     productName: "shoeds",
+  //     units: 12,
+  //     payment: 36,
+  //     transactionDate: "1-1-1977"
+  //   });
+  // }
 });
